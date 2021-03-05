3303fb811e677f10ce5b762ebcb833ac5f9904dd
nancylago <80120654+nancylago@users.noreply.github.com>
2021-03-05 14:25:16 -0300

    Add files via upload

---
+++ poema7.py
@@ -0,0 +1,175 @@
+print("***************************************************")
+print("Hola. Soy STR, una generadora automática de poemas.")
+nombre = str(input("¿Cuál es tu nombre o alias?   "))
+print("Hola,  " + nombre + ". Procederé con algunas preguntas.")
+print("El 11 de marzo de 2020, la OMS anunció que la enfermedad producida")
+print("por el COVID-19 podía caracterizarse como una pandemia")
+preg_uno = str(input("¿Qué imagen o sensación asocias a ese día?   "))
+preg_dos = int(input("Ahora, elige un número del 1 al 20:   "))
+preg_tres = str(input("¿Qué es lo que más extrañas de tu vida pre-Covid?   "))
+preg_cuatro = int(input("Ahora, elige un número del 21 al 40:   "))
+preg_cinco = int(input("Finalmente, elige un número del 41 al 60:   "))
+
+
+print("\n")
+print("*****************************")
+print("Aquí está tu poema automático")
+print("*****************************")
+
+print("\n")
+
+if preg_cinco == 41:
+    print("Sistema de ayuda")
+if preg_cinco == 42:
+    print("Bloqueos del plano")
+if preg_cinco == 43:
+    print("Diálogos en la interfaz")
+if preg_cinco == 44:
+    print("Estudio observacional")
+if preg_cinco == 45:
+    print("Factor de riesgo")
+if preg_cinco == 46:
+    print("Una revisión sistemática")
+if preg_cinco == 47:
+    print("Retirada de aislamiento")
+if preg_cinco == 48:
+    print("Entorno quirúrgico")
+if preg_cinco == 49:
+    print("Cohorte de pacientes")
+if preg_cinco == 50:
+    print("Adherencia al tratamiento")
+if preg_cinco == 51:
+    print("Gigante de pared")
+if preg_cinco == 52:
+    print("Lista de espera")
+if preg_cinco == 53:
+    print("A propósito de un caso")
+if preg_cinco == 54:
+    print("Consultas telemáticas")
+if preg_cinco == 55:
+    print("Única expresión")
+if preg_cinco == 56:
+    print("Eventos adversos")
+if preg_cinco == 57:
+    print("Posibilidad de sesgo")
+if preg_cinco == 58:
+    print("Estado de alarma")
+if preg_cinco == 59:
+    print("Protección personal")
+if preg_cinco == 60:
+    print("Relación bidireccional")
+if preg_cinco <41:
+    print("los números no existen")
+if preg_cinco >60:
+    print("los números no existen")
+
+
+print("\n")
+  
+
+print(preg_uno)
+if preg_dos == 1:
+    print("el cuerpo dejó de ser el puente")
+if preg_dos == 2:
+    print("el colapso que podríamos experimentar")
+if preg_dos == 3:
+    print("adaptación y aceptación") 
+if preg_dos == 4:
+    print("hacer un sacrificio compartido") 
+if preg_dos == 5:
+    print("legitimidad de las decisiones") 
+if preg_dos == 6:
+    print("que no acatan órdenes") 
+if preg_dos == 7:
+    print("esto no está ocurriendo") 
+if preg_dos == 8:
+    print("culpar a otros") 
+if preg_dos == 9:
+    print("un arranque híbrido") 
+if preg_dos == 10:
+    print("me parece un gran error") 
+if preg_dos == 11:
+    print("es importante no perder la perspectiva") 
+if preg_dos == 12:
+    print("una mayor tolerancia al riesgo") 
+if preg_dos == 13:
+    print("la renuncia a oponer la defensa") 
+if preg_dos == 14:
+    print("de cualquier naturaleza u origen") 
+if preg_dos == 15:
+    print("se teme, pero se anhela") 
+if preg_dos == 16:
+    print("el retorno de la indiferencia") 
+if preg_dos == 17:
+    print("rigidez casi absoluta") 
+if preg_dos == 18:
+    print("en medio del confinamiento") 
+if preg_dos == 19:
+    print("marzo fue un mes fatídico") 
+if preg_dos == 20:
+    print("en contra de su bienestar")
+if preg_dos <1:
+    print("olvidé el orden de los números")
+if preg_dos >20:
+    print("olvidé el orden de los números") 
+
+
+
+print(preg_tres)
+if preg_cuatro == 21:
+  print("cómo es el mundo hoy")
+if preg_cuatro == 22:
+    print("¿volveremos al mismo lugar?") 
+if preg_cuatro == 23:
+    print("posesión, uso, goce, disposición") 
+if preg_cuatro == 24:
+    print("un replanteamiento radical de la forma")
+if preg_cuatro == 25:
+    print("el destino exclusivo e irrevocable")
+if preg_cuatro == 26:
+    print("el estado rastrea los movimientos")
+if preg_cuatro == 27:
+    print("desplazamientos mínimos e indispensables")
+if preg_cuatro == 28:
+    print("medidas excepcionales y urgentes")
+if preg_cuatro == 29:
+    print("respuesta a inquietudes y miedos")
+if preg_cuatro == 30:
+    print("tres mil personas con hambre y miedo")
+if preg_cuatro == 31:
+    print("no hay una forma certera de saber")
+if preg_cuatro == 32:
+    print("un lugar difícil de transitar")
+if preg_cuatro == 33:
+    print("conseguir un recurso para vivir")
+if preg_cuatro == 34:
+    print("ola enorme de lento avance")
+if preg_cuatro == 35:
+    print("sucumbe a las consecuencias")
+if preg_cuatro == 36:
+    print("un desplazamiento del epicentro")
+if preg_cuatro == 37:
+    print("se convirtió en el principal temor")
+if preg_cuatro == 38:
+    print("la urgencia de continuar")
+if preg_cuatro == 39:
+    print("aprender a vivir con ella")
+if preg_cuatro == 40:
+    print("en el camino del miedo")
+if preg_cuatro <20:
+    print("no hay verdad en los números")
+if preg_cuatro >20:
+    print("no hay verdad en los números")
+
+print("\n")
+print("***************************************************")
+cierre = str(input("Para salir del programa, presiona cualquier tecla y luego 'enter'    "))
+if cierre == "s":
+    quit()
+else:
+    quit()
+    
+
+
+
+
